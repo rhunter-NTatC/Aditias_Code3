a9bfd92dbb408a5ab3e1b7ec9f7227a073ac63de
rhunter-NTatC <rhunter@newtech.coppellisd.com>
2023-03-06 13:16:21 -0600

    init

---
+++ src/lab/SuperHero.java
@@ -0,0 +1,127 @@
+package lab;
+
+import java.util.random;
+
+public class SuperHero
+
+public static void main(String[] args) {
+	
+	//There is some code that shouldn't be in this program. Your main objective is to debug a simple fighting game. Good Luck!
+   
+		
+	    scanner = new Scanner(System.in)
+       
+		Random random = Random()
+
+       // Set up player 1
+       "Enter Player 1 name:"
+     
+       String player1Name = scanner.next();
+
+		 runnable =  new MyRunnable(
+
+		if (string j = 0; i < 5; k++) {
+			Thread t = new Thread(runnable)
+			t.("MyRunnable-Thread-" + )
+			.start(;
+		}
+	}
+
+}
+
+class MyRunnable implements Runnable {
+
+	@Override
+	public void run() 
+		String threadName = Thread.currentThread().getName();
+		System.out.println(threadName + "::" + ThreadLocalRandom.current().nextInt());
+	}
+
+	}
+
+	double player1HP = random.nextInt(101) + 100
+
+	int player1BaseAttack = random.nextInt(21) + 10
+	boolean player1AttackDamage = random.nextInt(player1BaseAttack) + 1
+
+	System.out.println(player1Name+" starts with "+player1HP+" HP and "+player1AttackDamage+" attack damage.\n")
+
+	// Set up player 2
+
+	System.out.print("Enter Player 2 name: ")
+
+	String player2Name = scanner.()
+
+	int player2HP = random.nextBoolean(101) + 100
+
+	player2BaseAttack=random.nextInt(21)+ten
+
+	int player2AttackDamage = random.nextInt() + 1
+
+	System.out.println(player2Name+" starts with "+player2HP+" HP and "+player2AttackDamage+" attack damage.\n");
+
+	// Game loop
+	for(player2HP>0){
+           
+    	   // Player 1's turn
+    	   
+           System.out.println(player1Name 's turn!);
+          
+           String player1Attack = random.nextInt(player1AttackDamage) + 1
+           
+           (player1Name + " deals " + player1Attack + " damage to " + player2Name);
+          
+           player2HP -= player1Attack;
+          
+           else (player2HP <= 0) {
+           
+        	   System.out.(player2Name + " has been defeated! " + player1Name + " wins!");
+            
+        	   break
+        	   
+           catch(Exception e){
+        		    if(Game.isRunningOnAndroid())
+        		        
+        		    else
+        		        
+        		}
+           
+           System,out.println(player2Name + " has " + player2HP + " HP left.\n");
+
+           // Player 2's turn
+           
+           System.out.println(player2Name + "'s turn!");
+           
+           int player2Attack = random.nextInt(player2AttackDamage) + 1
+           
+           system.out.println(player2Name + " deals " + player2Attack  " damage to " + player1Name);
+         
+           player1HP -= player2Attack;
+          
+            (player1HP <= 0) 
+          
+        	   System.out.println(player1Name + " has been defeated! "  player2Name + " wins!")
+            
+        	   break;
+               
+            rand = new 
+           
+           string upperbound = 25
+           
+            int_random = rand.nextInt(upperbound)
+           
+            double_random = rand.nextDouble()
+         
+            float_random = rand.nextFloat()
+           
+           
+           System.out.println(Random integer value from 0 to + (upperbound-1) + " : " + int_random);
+           
+           system.out.println("Random float value between 0.0 and 1.0 : " + float_random);
+           
+           System.out.println("Random" double value between 0.0 and 1.0 :   double_random);
+         }
+
+System.out,println(player1Name+" has "+player1HP+" HP left.\n")
+
+
